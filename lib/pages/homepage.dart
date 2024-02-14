fbed6f3068853c1957afc8adc5d3e4d27a9e5cf4
Gursahib Singh <gursahib99888@gmail.com>
2024-02-14 05:03:14 -0800

    added Drawer

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -93,133 +93,143 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'U N I V O L V E',
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-            color: Colors.white,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'U N I V O L V E',
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.white,
+            ),
           ),
+          // leading: IconButton(
+          //   icon:
+          //       Icon(Icons.menu, color: Colors.white), // Custom icon and color
+          //   onPressed: () {
+          //     Scaffold.of(context).openDrawer();
+          //   },
+          // ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              color: Colors.white,
+              onPressed: () {},
+            ),
+          ],
+          centerTitle: true,
+          backgroundColor: Color(0xff016D77),
+          elevation: 0,
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            color: Colors.white,
-            onPressed: () {},
-          ),
-        ],
-        centerTitle: true,
-        backgroundColor: Color(0xff016D77),
-        elevation: 0,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: <Color>[
-                    Color(0xff016D77),
-                    Color(0xff84C5BE),
+        drawer: Drawer(
+          backgroundColor: Color(0xff84C5BE),
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: <Color>[
+                      Color(0xff016D77),
+                      Color(0xff84C5BE),
+                    ],
+                  ),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage('assets/images/profile.jpg'),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "$userEmail ",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
                   ],
                 ),
               ),
-              child: Column(
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage: AssetImage('assets/images/profile.jpg'),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "$userEmail ",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+              ListTile(
+                title: Text('Profile'),
+                leading: Icon(UnivolveIcons.profile),
+                onTap: () {
+                  Navigator.pushNamed(context, '/profile');
+                },
               ),
-            ),
-            ListTile(
-              title: Text('Profile'),
-              leading: Icon(UnivolveIcons.profile),
-              onTap: () {
-                Navigator.pushNamed(context, '/profile');
-              },
-            ),
-            ListTile(
-              title: Text('Settings'),
-              leading: Icon(Icons.settings),
-              onTap: () {
-                Navigator.pushNamed(context, '/settings');
-              },
-            ),
-            ListTile(
-              title: Text('About'),
-              leading: Icon(Icons.info),
-              onTap: () {
-                Navigator.pushNamed(context, '/about');
-              },
-            ),
-            ListTile(
-              title: Text('Sign Out'),
-              leading: Icon(Icons.exit_to_app),
-              onTap: () {
-                FirebaseAuth.instance.signOut();
-              },
-            ),
-          ],
-        ),
-      ),
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-          child: GNav(
-            selectedIndex: _selectedIndex,
-            gap: 8,
-            iconSize: 24,
-            haptic: true,
-            color: Color(0xff676D75),
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-            activeColor: Color(0xff016D77),
-            tabBackgroundColor: Color(0xff84C5BE).withOpacity(0.4),
-            onTabChange: (index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            tabs: [
-              GButton(
-                icon: UnivolveIcons.chat,
-                text: '',
+              ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings),
+                onTap: () {
+                  Navigator.pushNamed(context, '/settings');
+                },
               ),
-              GButton(
-                icon: UnivolveIcons.notification,
-                text: '',
+              ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.info),
+                onTap: () {
+                  Navigator.pushNamed(context, '/about');
+                },
               ),
-              GButton(
-                icon: UnivolveIcons.home,
-                text: '',
-              ),
-              GButton(
-                icon: UnivolveIcons.events,
-                text: '',
-              ),
-              GButton(
-                icon: UnivolveIcons.profile,
-                text: '',
+              ListTile(
+                title: Text('Sign Out'),
+                leading: Icon(Icons.exit_to_app),
+                onTap: () {
+                  FirebaseAuth.instance.signOut();
+                },
               ),
             ],
+          ),
+        ),
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+            child: GNav(
+              selectedIndex: _selectedIndex,
+              gap: 8,
+              iconSize: 24,
+              haptic: true,
+              color: Color(0xff676D75),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              activeColor: Color(0xff016D77),
+              tabBackgroundColor: Color(0xff84C5BE).withOpacity(0.4),
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              tabs: [
+                GButton(
+                  icon: UnivolveIcons.chat,
+                  text: '',
+                ),
+                GButton(
+                  icon: UnivolveIcons.notification,
+                  text: '',
+                ),
+                GButton(
+                  icon: UnivolveIcons.home,
+                  text: '',
+                ),
+                GButton(
+                  icon: UnivolveIcons.events,
+                  text: '',
+                ),
+                GButton(
+                  icon: UnivolveIcons.profile,
+                  text: '',
+                ),
+              ],
+            ),
           ),
         ),
       ),
